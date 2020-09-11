9665836b4c9c9f79be00c83bbcfa156a3e166414
Carles <carles@crix.design>
2020-09-11 13:30:03 +0200

    fixed conflicts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 
 import App from './App.jsx';
 import './css/App.css';
 
 const elem = document.querySelector('.root');
 
-const wrapApp = AppComponent => (
+const wrapApp = (AppComponent) => (
   <Router>
     <AppComponent />
   </Router>
@@ -16,4 +16,4 @@ const wrapApp = AppComponent => (
 
 ReactDOM.render(wrapApp(App), elem);
 
-registerServiceWorker()
+// registerServiceWorker()
